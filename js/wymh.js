afb977a256389a9d6df127071d8077d8f79208ed
omrun <zycyeh@gmail.com>
2019-05-17 23:17:51 +0800

    ~

---
+++ js/wymh.js
@@ -0,0 +1,4 @@
+//网易漫画
+//api.mh.163.com
+http-response https?:\/\/api\.mh\.163.com\/getUserProfile script-path=https://raw.githubusercontent.com/MeetaGit/MeetaRules/master/Surge/Scripting/wymh.js
+
